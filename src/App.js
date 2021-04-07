3624e1506c08736d0b37991cf980d7f334b6bfbd
Naman-1234 <namankalrabhiwani54@gmail.com>
2021-04-07 22:23:17 +0530

    Added Education Page

--- src/App.js
+++ src/App.js
@@ -3,14 +3,13 @@ import Navbar from './Components/Navbar/Navbar';
 import "./Components/FrontPage/FrontPage";
 import FrontPage from './Components/FrontPage/FrontPage';
 import './App.css';
-import AOS from 'aos';
-import 'aos/dist/aos';
-AOS.init();
+import Education from './Components/Education/Education';
 function App() {
   return (
     <div>
     <Navbar/>
     <FrontPage />
+    <Education />
     </div>
   );
 }
---
+++ src/Components/Utilities/EducationCard/EducationCard.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./EducationCard.scss";
+function EducationCard(props) {
+    const {Institute, Class, Year, Grade} = props
+    return (
+        <div className="education-card" data-aos="zoom-in" data-duration="2000">
+            <h2 className="heading2">{Institute}</h2>
+            <h4 className="heading4">{Class}</h4>
+
+            {Grade !=='' &&<h4 className="heading4">{Grade}</h4>}
+            <h4 className="heading4">{Year}</h4>
+        </div>
+    )
+}
+
+export default EducationCard
