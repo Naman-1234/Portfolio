f2fbd45e9829dbb467026cb2b2f686ceab52b8c9
Naman-1234 <namankalrabhiwani54@gmail.com>
2021-04-16 23:45:53 +0530

    Improved Code Structure in Education File

--- src/Components/Education/Education.jsx
+++ src/Components/Education/Education.jsx
@@ -1,26 +1,7 @@
 import React from "react";
 import EducationCard from "../Utilities/EducationCard/EducationCard";
 import "./Education.scss";
-const educationData = [
-  {
-    Institute: "Indian Institute of Information Technology Vadodara",
-    Class: "Bachelor Of Technology (B.Tech) in Computer Science",
-    Year: "2019-2023",
-    Grade: "",
-  },
-  {
-    Institute: "Gr. Senior Secondary  School",
-    Class: "Sr. Secondary Education (Class: 12)",
-    Year: "2018-2019",
-    Grade: "Percentage Obtained: 93%",
-  },
-  {
-    Institute: "Bhiwani Public  School",
-    Class: "SecondaryEducation (Class: 10)",
-    Year: "2016-2017",
-    Grade: "CGPA: 10",
-  },
-];
+const educationData =  require('../../Data/EducationData.json');
 function Education() {
   return (
     <div className="education">
