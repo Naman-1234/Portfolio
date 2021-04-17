b4dde1f76c921a7c06463b7bd2863a64826d3fe2
Naman-1234 <namankalrabhiwani54@gmail.com>
2021-04-17 21:08:39 +0530

    Footer work in progress

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -12,36 +12,34 @@ function Contact() {
       </div>
       <div className="contact__description">
         <div className="up">
-        <div className="up__left">
-        <form >
-        <div className="name">
-            <label htmlFor="name">Name:</label>
-            <input type="text" id="name" required></input>
-        </div>
-        <div className="email">
-            <label htmlFor="name">Email:</label>
-            <input type="email" id="email"></input>
+          <div className="up__left">
+            <div className="name">
+              <label htmlFor="name">Name:</label>
+              <input type="text" id="name" required></input>
+            </div>
+            <div className="email">
+              <label htmlFor="name">Email:</label>
+              <input type="email" id="email"></input>
             </div>
             <div className="description_label">
-            <label htmlFor="description">Description:</label>
+              <label htmlFor="description">Description:</label>
             </div>
             <div className="description__text">
-            <textarea cols="50" rows="10" style={{resize:"none"}}>Enter your message here</textarea>
+              <textarea cols="50" rows="10" style={{ resize: "none" }}>
+                Enter your message here
+              </textarea>
             </div>
             <div className="submit_button">
-                <button type="button" role="button">Send</button>
+              <button>Send</button>
             </div>
-            </form>
-        </div>
-     
-        <div className="up__right">
+          </div>
 
-        </div>
+          <div className="up__right"></div>
         </div>
         <div className="down">
-        <p>Made with ❤️ by Naman</p>
+          <p>Made with ❤️ by Naman</p>
         </div>
-    </div>
+      </div>
     </div>
   );
 }
