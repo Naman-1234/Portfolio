20d44ae7a00cfb9464e82e1759a4a57e22853a3a
Naman-1234 <namankalrabhiwani54@gmail.com>
2021-05-05 18:39:23 +0530

    Added PreLoader

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import {React,useState,useEffect} from 'react';
+import Loader from "react-loader-spinner";
 import Navbar from './Components/Navbar/Navbar';
 import "./Components/FrontPage/FrontPage";
 import FrontPage from './Components/FrontPage/FrontPage';
@@ -7,9 +8,33 @@ import Education from './Components/Education/Education';
 import Project from "./Components/Project/Project";
 import Skills from "./Components/Skills/Skills";
 import Contact from "./Components/Contact/Contact";
+
 function App() {
+  const [loaded,setloaded] = useState(false);
+  useEffect(() => {
+    const update= ()=>{
+    // setTimeout(()=>{setloaded(true)},3000);
+    setloaded(true);
+    }
+    update();
+  },);
+  if(!loaded) {
+  return (
+    <div className="loader">
+    <Loader
+    type="Puff"
+    color="#00BFFF"
+    height={100}
+    width={100}
+    timeout={3000} //3 secs
+  />
+  </div>
+  )
+  }
+  if(loaded) {
   return (
     <div>
+ 
     <Navbar/>
     <FrontPage />
     <Education />
@@ -17,7 +42,8 @@ function App() {
     <Skills />
     <Contact />
     </div>
-  );
+  )
+  }
 }
 
 export default App;
