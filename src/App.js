6b71b0c80f9bf97fa89d3eee2fcd446a57435b3d
Naman-1234 <namankalrabhiwani54@gmail.com>
2021-05-05 18:43:39 +0530

    Preloaded conditionally rendered

--- src/App.js
+++ src/App.js
@@ -1,48 +1,42 @@
-import {React,useState,useEffect} from 'react';
+import { React, useState, useEffect } from "react";
 import Loader from "react-loader-spinner";
-import Navbar from './Components/Navbar/Navbar';
+import Navbar from "./Components/Navbar/Navbar";
 import "./Components/FrontPage/FrontPage";
-import FrontPage from './Components/FrontPage/FrontPage';
-import './App.css';
-import Education from './Components/Education/Education';
+import FrontPage from "./Components/FrontPage/FrontPage";
+import "./App.css";
+import Education from "./Components/Education/Education";
 import Project from "./Components/Project/Project";
 import Skills from "./Components/Skills/Skills";
 import Contact from "./Components/Contact/Contact";
 
 function App() {
-  const [loaded,setloaded] = useState(false);
+  const [loaded, setloaded] = useState(false);
   useEffect(() => {
-    const update= ()=>{
-    // setTimeout(()=>{setloaded(true)},1500);
-    }
+    const update = () => {
+      setTimeout(() => {
+        setloaded(true);
+      }, 800);
+    };
     update();
-  },);
-  if(!loaded) {
-  return (
-    <div className="loader">
-    <Loader
-    type="Puff"
-    color="#00BFFF"
-    height={100}
-    width={100}
-    timeout={3000} //3 secs
-  />
-  </div>
-  )
-  }
-  if(loaded) {
+  });
   return (
     <div>
- 
-    <Navbar/>
-    <FrontPage />
-    <Education />
-    <Project />
-    <Skills />
-    <Contact />
+      {!loaded ? (
+        <div className="loader">
+          <Loader type="BallTriangle" color="#00BFFF" height={80} width={80} />
+        </div>
+      ) : (
+        <div>
+          <Navbar />
+          <FrontPage />
+          <Education />
+          <Project />
+          <Skills />
+          <Contact />
+        </div>
+      )}
     </div>
-  )
-  }
+  );
 }
 
 export default App;
