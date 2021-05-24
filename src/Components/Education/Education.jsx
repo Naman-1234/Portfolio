9ccc4559b4a283da3e8cb122ba55acbf96568d24
Naman <namankalrabhiwani54@gmail.com>
2021-05-24 23:56:33 +0530

    Redesigned Education UI

--- src/Components/Education/Education.jsx
+++ src/Components/Education/Education.jsx
@@ -1,33 +1,33 @@
-import React from "react";
-import EducationCard from "../Utilities/EducationCard/EducationCard";
-import "./Education.scss";
-const educationData =  require('../../Data/EducationData.json');
+import React from 'react';
+import EducationCard from '../Utilities/EducationCard/EducationCard';
+import './Education.scss';
+const educationData = require('../../Data/EducationData.json');
 function Education() {
   return (
-    <div className="education" id="educationPage">
+    <div className='education' id='educationPage'>
       <div
-        className="education__heading"
-        data-aos="zoom-in-right"
-        data-duration="2000"
+        className='education__heading'
+        data-aos='zoom-in-right'
+        data-duration='2000'
       >
         <h1>Education</h1>
       </div>
-      <div className="education__description">
-      <div className="description__left">
-      <img src="images/Education2.png" alt="Education" />
-      </div>
-      <div className="description__right">
-        {educationData.map((data, index) => {
-          return (
-            <EducationCard
-              Institute={data.Institute}
-              Class={data.Class}
-              Grade={data.Grade}
-              Year={data.Year}
-              key={index}
-            />
-          );
-        })}
+      <div className='education__description'>
+        <div className='description__left'>
+          <img src='images/Education2_without_bg.png' alt='Education' />
+        </div>
+        <div className='description__right'>
+          {educationData.map((data, index) => {
+            return (
+              <EducationCard
+                Institute={data.Institute}
+                Class={data.Class}
+                Grade={data.Grade}
+                Year={data.Year}
+                key={index}
+              />
+            );
+          })}
         </div>
       </div>
     </div>
