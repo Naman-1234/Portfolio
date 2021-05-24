f9715f4639d9c51bb71a1b18efb22cb85cb98e2a
Naman <namankalrabhiwani54@gmail.com>
2021-05-25 00:11:55 +0530

    Redesigned Contact Page UI

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,43 +1,51 @@
-import React from "react";
-import "./Contact.scss";
+import React from 'react';
+import './Contact.scss';
 function Contact() {
   return (
-    <div className="contact" id="contactPage">
+    <div className='contact' id='contactPage'>
       <div
-        className="contact__heading"
-        data-aos="zoom-in-right"
-        data-duration="2000"
+        className='contact__heading'
+        data-aos='zoom-in-right'
+        data-duration='2000'
       >
         <h1>Contact Me</h1>
       </div>
-      <div className="contact__description" data-aos="zoom-in-right" data-aos-duration="2000">
-        <div className="up">
-          <div className="up__left">
-            <div className="name">
-              <label htmlFor="name">Name:</label>
-              <input type="text" id="name" required></input>
+      <div
+        className='contact__description'
+        data-aos='zoom-in-right'
+        data-aos-duration='2000'
+      >
+        <div className='up'>
+          <div className='up__left'>
+            <div className='name'>
+              <label htmlFor='name'>Name:</label>
+              <input type='text' id='name' required></input>
             </div>
-            <div className="email">
-              <label htmlFor="name">Email:</label>
-              <input type="email" id="email"></input>
+            <div className='email'>
+              <label htmlFor='name'>Email:</label>
+              <input type='email' id='email'></input>
             </div>
-            <div className="description_label">
-              <label htmlFor="description">Description:</label>
+            <div className='description_label'>
+              <label htmlFor='description'>Description:</label>
             </div>
-            <div className="description__text">
-              <textarea cols="50" rows="10" style={{ resize: "none" }} placeholder="Enter your message here">
-              </textarea>
+            <div className='description__text'>
+              <textarea
+                cols='50'
+                rows='10'
+                style={{ resize: 'none' }}
+                placeholder='Enter your message here'
+              ></textarea>
             </div>
-            <div className="submit_button">
+            <div className='submit_button'>
               <button>Send</button>
             </div>
           </div>
 
-          <div className="up__right">
-    <img src="images/Contact.png" alt="Contact"/>
+          <div className='up__right'>
+            <img src='images/Contact_without_bg.png' alt='Contact' />
+          </div>
         </div>
-        </div>
-        <div className="down">
+        <div className='down'>
           <p>Made with ❤️ by Naman</p>
         </div>
       </div>
