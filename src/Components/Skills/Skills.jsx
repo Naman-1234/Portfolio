63f6880bb82636e93b6c1f049010f29b1a37d9fa
Naman <namankalrabhiwani54@gmail.com>
2021-05-25 00:05:19 +0530

    Skills and Projects Portion UI Redesgined

--- src/Components/Skills/Skills.jsx
+++ src/Components/Skills/Skills.jsx
@@ -1,28 +1,37 @@
-import React from 'react'
-import "./Skills.scss";
+import React from 'react';
+import './Skills.scss';
 
 const skillsLogos = require('../../Data/Skills.json');
 console.log('Skillslogo', skillsLogos);
 function Skills() {
-    return (
-        <div className="skills" id="skillsPage">
-            <div className="skills__heading">
-            <h1>Skills</h1>
-            </div>
-            <div className="skills__description" data-aos="zoom-in-right" data-aos-duration="2000">
-            <div className="description__left">
-            <img src="images/Skills.png" alt="Skills" />
-            </div>
-            <div className="description__right">
-            {
-                skillsLogos.map((src, index)=>{
-                    return <img src={src.src} alt="logo" key={index} />
-                })
-            }
-            </div>
-            </div>
+  return (
+    <div className='skills' id='skillsPage'>
+      <div className='skills__heading'>
+        <h1>Skills</h1>
+      </div>
+      <div
+        className='skills__description'
+        data-aos='zoom-in-right'
+        data-aos-duration='2000'
+      >
+        <div className='description__left'>
+          <img src='images/Skills_without_bg.png' alt='Skills' />
         </div>
-    )
+        <div className='description__right'>
+          {skillsLogos.map((src, index) => {
+            return (
+              <img
+                src={src.src}
+                alt='logo'
+                key={index}
+                className='language-img'
+              />
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Skills
+export default Skills;
